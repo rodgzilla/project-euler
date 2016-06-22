28346a8c8576d903822d7615db6f4c45e8514e7b
Grégory Chatel <chatel.gregory@gmail.com>
2016-06-22 21:29:01 +0200

    Problem 66.

--- problem_066/problem.py
+++ problem_066/problem.py
@@ -1,6 +1,14 @@
 import math
 from fractions import Fraction as frac
 
+def generate_development(seq):
+    seq_period = seq[1:]
+    yield seq[0]
+    i = 0
+    while True:
+        yield seq_period[i]
+        i = (i + 1) % len(seq_period)
+
 def continued_fraction(n):
     initial_term = math.floor(math.sqrt(n))
     terms = [initial_term]
@@ -25,8 +33,29 @@ def compute_sum(sequence):
     current += sequence[0]
     return current
 
-seq = [1] + [2] * 10
-for i in range(1, len(seq)):
-    print(compute_sum(seq[:i]))
-# squares = {i * i for i in range(101)}
-# print(sum([1 if len(continued_fraction(n)) % 2 == 0 else 0 for n in range(2, 10001) if n not in squares]))
+def solve_eq(d):
+    x, y = 0, 0
+    seq_size = 2
+    reduced_sequence = continued_fraction(d)
+    while x**2 - d * y ** 2 != 1:
+        generalized_sequence = generate_development(reduced_sequence)
+        sequence = [generalized_sequence.send(None) for _ in range(seq_size)]
+        current = 0
+        for i in range(len(sequence) - 1, 0, -1):
+            current = frac(1, sequence[i] + current)
+        current += sequence[0]
+        x = current.numerator
+        y = current.denominator
+        seq_size += 1
+    return x, y
+
+
+squares = {i * i for i in range(1, 33)}
+max_d = -1
+max_x_value = -1
+for d in (x for x in range(2, 1001) if x not in squares):
+    x, _ = solve_eq(d)
+    if x > max_x_value:
+        max_d = d
+        max_x_value = x
+print(max_d)
