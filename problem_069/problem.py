54eb9ef59a3551e21c1c23b6f3fa7d73ead4c665
Grégory Châtel <gchatel@univ-mlv.fr>
2016-06-23 11:17:46 +0200

    Problem 69.

--- problem_069/problem.py
+++ problem_069/problem.py
@@ -1,24 +1,25 @@
+from math import sqrt
+from collections import defaultdict
 from fractions import Fraction
 
-def prime_number_decomposition(n):
-    dec = []
-    div = 2
-    while n != 1:
-        if n % div == 0:
-            dec.append(div)
-            n //= div
-        else:
-            div += 1
-    return dec
+def reverse_erathos(n):
+    d = defaultdict(set)
+    for i in range(2, int(sqrt(n)) + 2):
+        if i not in d:
+            j = i
+            while j <= n:
+                d[j].add(i)
+                j += i
+    return d
 
-def totient(n):
+def totient(n, prime_decs):
     res = n
-    for prime in set(prime_number_decomposition(n)):
+    for prime in prime_decs[n]:
         res *= 1 - Fraction(1, prime)
     return int(res)
 
 def compute_solution():
-    return max([(n, Fraction(n, totient(n))) for n in range(2, 1000001)], key=lambda x:x[1])
-
+    prime_decs = reverse_erathos(1000000)
+    return max([(n, Fraction(n, totient(n, prime_decs))) for n in range(2, 1000001)], key=lambda x:x[1])
 
 print(compute_solution())
