b3595e435cb3161f5a3ce20e6c1c6d7407204ccc
Grégory Chatel <chatel.gregory@gmail.com>
2016-07-10 16:42:22 +0200

    Non stupid version.

--- problem_357/problem.py
+++ problem_357/problem.py
@@ -4,37 +4,19 @@ with open('../tools/prime_number_set', 'rb') as f:
     primes = pickle.load(f)
     print('loading done')
 
-valid_numbers = set(range(1, 100000001))
+valid_numbers = [True] * 100000001
 i = 1
 j = 1
 prod = 1
-print(i)
 while prod <= 100000000 or j != 1:
     if prod > 100000000 or i < j:
-        print(i)
         i += 1
         j = 1
         prod = i * j
         continue
     if i + j not in primes:
-        valid_numbers.discard(prod)
+        valid_numbers[prod] = False
     j += 1
     prod = i * j
-print(sum(valid_numbers))
+print(sum([i for i, val in enumerate(valid_numbers) if val]))
 
-
-# for i in range(1, 10001):
-#     print(i)
-#     if i % 2 == 0:
-#         inc = 1
-#         j = 1
-#     else:
-#         inc = 2
-#         j = 2
-#     prod = i * j
-#     while prod < 100000000:
-#         if i + j not in primes:
-#             non_valid.add(prod)
-#         j += inc
-#         prod = i * j
-# print((100000000 * 100000001) // 2 - (49999999 + 1) ** 2 - sum(non_valid))
