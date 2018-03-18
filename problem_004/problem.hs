8f2d926ee182adc0096b3683f9da6911aa777bd9
Grégory Châtel <chatel.gregory@gmail.com>
2018-03-18 13:59:59 +0100

    More element solution to problem 4 in Haskell.

--- problem_004/problem.hs
+++ problem_004/problem.hs
@@ -6,4 +6,11 @@ isPalindromeString s
   | otherwise        = False
 
 answer :: Int
-answer = maximum [x * y | x <- [999,998..0], y <- [999,998..0], isPalindromeString . show $ x * y]
+answer = maximum [x * y | x <- [0..999], y <- [0..999], isPalindromeString . show $ x * y]
+
+answer' :: Int
+answer' = maximum [x * y | x <- [0..999], y <- [0..999], (reverse . show $ x * y) == (show $ x * y)]
+
+-- Solution from https://wiki.haskell.org/Euler_problems/1_to_10
+answer'' :: Int
+answer'' = maximum [x | y <- [100..999], z <- [100..999], let x = y * z, let s = show x, s == reverse s]
